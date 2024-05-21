42e0501ec0dc684acfac94f904e7c01431ae648e
kuppaiahsaikiran <112300858+kuppaiahsaikiran@users.noreply.github.com>
2024-05-21 17:58:37 +0530

    Delete productproject directory

--- productproject/src/main/java/com/kodnest/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodnest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kodnest.entity.Laptop;
-
-public interface LaptopRepository extends JpaRepository<Laptop, Integer> {
-
-}
